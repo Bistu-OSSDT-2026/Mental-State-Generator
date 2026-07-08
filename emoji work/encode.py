fb72732b961c70f1e0df85bd19289f61768841a6
Luo927 <wang10147654@gmail.com>
2026-07-08 23:06:48 +0800

    Delete emoji work directory

--- emoji work/encode.py
+++
@@ -1,7 +0,0 @@
-import base64
-f = open('images/bistu-logo.png', 'rb')
-data = f.read()
-f.close()
-encoded = base64.b64encode(data).decode()
-print(encoded[:100])
-print('Total length:', len(encoded))
--- emoji work/encode2.py
+++
@@ -1,9 +0,0 @@
-import base64
-f = open('images/bistu-logo.png', 'rb')
-data = f.read()
-f.close()
-encoded = base64.b64encode(data).decode()
-f2 = open('output.txt', 'w')
-f2.write(encoded)
-f2.close()
-print('Done')
